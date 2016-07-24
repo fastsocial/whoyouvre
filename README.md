3b94eebedb33a7c919356afc66c1606158119b94
Ivan Kuznetsov <kuznecov.fd0@yandex.ru>
2016-07-24 17:33:20 +0300

    Первый нах

---
+++ README.md
@@ -0,0 +1,2 @@
+Whouyvre project!
+
---
+++ frontend/admin/index.php
@@ -0,0 +1,12 @@
+<!doctype html>
+<html>
+    <head>
+        <script src="https://code.jquery.com/jquery-2.2.4.min.js" type="text/javascript"></script>
+        <script src="https://code.jquery.com/ui/1.12.0/jquery-ui.min.js" type="text/javascript"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/raphael/2.2.1/raphael.min.js" type="text/javascript"></script>
+        <link rel="stylesheet" type="text/css" href="/css/style.css">
+    </head>
+    <body>
+        <h1>Whouyvre!</h1>
+    </body>
+</html>
